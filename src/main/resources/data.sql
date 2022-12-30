6206269cb90624bd725e8c39015fec0c9be176ce
gtn777 <gtn777@orange.fr>
2022-12-30 21:09:50 +0100

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@ insert into Users(fullname, username, password, role) values("Administrator", "a
 insert into Users(fullname, username, password, role) values("User", "user", "$2a$10$NTMzJWnIDQyV62VMWR.CMuVedBFx0ob7XNtVQmG3B7TyHaj8eu30e", "USER");
 insert into Users(fullname, username, password, role) values("Bob", "bob", "$2a$10$E1ovBtxyvpgcRptBB/Uo0.lbJpLdfJvWx642OVeltzcpZ/6YF024m", "USER");
 insert into Users(fullname, username, password, role) values("Admin", "admin", "$2a$10$E1ovBtxyvpgcRptBB/Uo0.lbJpLdfJvWx642OVeltzcpZ/6YF024m", "ADMIN");
+insert into BidList(account, type, bidQuantity) values("HSBC", "type 1", 25);
+insert into BidList(account, type, bidQuantity) values("BNP", "Offshore", 40);
+insert into BidList(account, type, bidQuantity) values("HSBC", "type 7", 12);
